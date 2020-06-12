38e33439810ddaca2ffcf06df2f52a2849ab62ed
my5t3ry <sash@mischkonsum.org>
2020-06-13 01:34:55 +0200

    init

---
+++ update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp -r ./controllers/* /Users/my5t3ry/Library/Application Support/Mixxx/controllers/
+rm -rf /Applications/Mixxx.app/Contents/Resources/skins/pragmatic
+cp -r ./skin/pragmatic /Applications/Mixxx.app/Contents/Resources/skins/
