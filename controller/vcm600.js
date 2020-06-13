3fdb8cc89dd761eb0ef984a18dcce44b0cd337c9
my5t3ry <sash@mischkonsum.org>
2020-06-13 05:30:48 +0200

    init

--- controller/vcm600.js
+++ controller/vcm600.js
@@ -266,7 +266,7 @@ var toggleLoop = function (channel, control, value, status, group) {
             midi.sendShortMsg("0x" + (90 + channel), 0x41, 0x7F);
             vcm600.loopActive[group] = 1;
         } else {
-            engine.setValue(group, "beatloop_4_toggle", 1);
+            engine.setValue(group, "beatloop_activate", 0);
             vcm600.loopActive[group] = 0;
             midi.sendShortMsg("0x" + (90 + channel), 0x41, 0x00);
         }
