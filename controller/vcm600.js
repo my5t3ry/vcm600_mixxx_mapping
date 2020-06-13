6503cb98dbc3e1b0cb152fd17b12b08df6c30558
my5t3ry <sash@mischkonsum.org>
2020-06-13 03:16:35 +0200

    init

--- controller/vcm600.js
+++ controller/vcm600.js
@@ -291,13 +291,13 @@ var play = function (channel, control, value, status, group) {
 
 var breakDeck = function (channel, control, value, status, group) {
     if (vcm600.selectedGroup != "[Library]") {
-        engine.setValue("vcm600.selectedGroup", "rate_temp_down", 1);
+        engine.setValue(vcm600.selectedGroup, "rate_temp_down", 1);
     }
 }
 
 var accelerateDeck = function (channel, control, value, status, group) {
     if (vcm600.selectedGroup != "[Library]") {
-        engine.setValue("vcm600.selectedGroup", "rate_temp_up", 1);
+        engine.setValue(vcm600.selectedGroup, "rate_temp_up", 1);
     }
 }
 
